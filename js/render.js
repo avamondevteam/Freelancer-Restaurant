c65022e78263a00a936250c6a3d71d52166581d0
PhamCong01 <phamvancong300701@gmail.com>
2023-03-09 10:05:32 +0700

    update

--- js/render.js
+++ js/render.js
@@ -1,4 +1,3 @@
-
 var _$ = document.querySelector.bind(document);
 var $$ = document.querySelectorAll.bind(document);
 const buttonSushi = _$(".button-sushi");
@@ -12,17 +11,142 @@ const images = $$(".menus .buttonImages");
 const overlay = _$(".container .overlay");
 const slickSlider = _$("#slick-slider");
 
-buttonSushi.addEventListener("click", function (e) {
-  _$(".toggle-appetizer").style.display = "none";
-  _$(".toggle-sushi").style.display = "block";
-});
+buttonSushi.addEventListener("click", async function (e) {
+  try {
+    await $.getJSON("db/sushi.json", (data) => {
+      const elementSushi = data
+        .map(
+          (value) => `
+        <div class="menus col-md-6 col-lg-4 d-flex">
+            <div class="menu-img img"
+            onclick="getImages('{{${value.images}}}')"
+            style="background-image: url(${value.images[0]});"></div>
+          <div class="text">
+            <div class="">
+              <h6 style="color: #E52B34;">${value.name}</h6>
+            </div>
+            ${value.ingredient
+              .map(
+                (value) => `<div class="d-flex justify-content-between">
+            <p><span style="font-size: 12px">${value.name}</span></p>
+            <div class="one-forth">
+              <span style="font-size: 12px !important;" class="price">${value.calo}</span>
+            </div>
+          </div>`
+              )
+              .join("")}
+          </div>
+        </div>
+  `
+        )
+        .join("");
 
+      document.querySelector(".toggle-sushi .menu-wrap .sushi").innerHTML =
+        elementSushi;
+      _$(".toggle-appetizer").style.display = "none";
+      _$(".toggle-sushi").style.display = "block";
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+$.getJSON('db/appetizer.json', (data => {
+  const elementAppetizer = data.map(value => {
+
+    return  `
+    <div class="menus col-md-6 col-lg-4 d-flex">
+      <div class="menu-img img" 
+      onclick="getImages('{{${value.images}}}')"
+      style="background-image: url(${value.images[0]});"></div>
+      <div class="text">
+        <div class="d-flex justify-content-between">
+          <h6 style="color: #E52B34;">${value.name}</h6>
+          <div class="one-forth">
+            <span style="font-size: 12px !important;" class="price">${value.calo}</span>
+          </div>
+        </div>
+        
+          ${value.description.map(data => {
+              return `<div class=""><p><span style="font-size: 12px;">${data}</span></p></div>`
+            }).join('')}
+        
+      </div>
+    </div>
+    `
+  }).join('')
+  document.querySelector('.appetizer').innerHTML = elementAppetizer
+}))
 buttonAppetizer.addEventListener("click", function (e) {
+  $.getJSON('db/appetizer.json', (data => {
+    const elementAppetizer = data.map(value => {
+
+      return  `
+      <div class="menus col-md-6 col-lg-4 d-flex">
+        <div class="menu-img img" 
+        onclick="getImages('{{${value.images}}}')"
+        style="background-image: url(${value.images[0]});"></div>
+        <div class="text">
+          <div class="d-flex justify-content-between">
+            <h6 style="color: #E52B34;">${value.name}</h6>
+            <div class="one-forth">
+              <span style="font-size: 12px !important;" class="price">${value.calo}</span>
+            </div>
+          </div>
+          
+            ${value.description.map(data => {
+                return `<div class=""><p><span style="font-size: 12px;">${data}</span></p></div>`
+              }).join('')}
+          
+        </div>
+      </div>
+      `
+    }).join('')
+    document.querySelector('.appetizer').innerHTML = elementAppetizer
+  }))
   _$(".toggle-sushi").style.display = "none";
   _$(".toggle-appetizer").style.display = "block";
 });
 
 buttonSoup.addEventListener("click", function (e) {
+  $.getJSON("db/soup.json", (data) => {
+    const elementSoup = data
+      .map((value) => {
+        return `
+      <div class="menus col-md-6 col-lg-4 d-flex ">
+        <div class="menu-img img" 
+        onclick="getImages('{{${value.images}}}')"
+        style="background-image: url(${value.images[0]});"></div>
+        <div class="text">
+          <div class="d-flex justify-content-between">
+          <div class="">
+            <h6 style="color: #E52B34;">${value.name}</h6>
+          </div>
+          <div class="one-forth">
+            <span style="font-size: 12px !important;" class="price">${
+              value.calo
+            }</span>
+          </div>
+        </div>
+          ${value.description
+            .map((data) => {
+              return `
+            <div class="d-flex justify-content-between">
+              <p><span style="font-size: 12px;">${data.name}</span></p>
+              
+              <div class="one-forth">
+                <span style="font-size: 12px !important;" class="price">${data.calo}</span>
+              </div>
+            </div>
+            `;
+            })
+            .join("")}
+        </div>
+      </div>
+      `;
+      })
+      .join("");
+    document.querySelector(".menu-wrap .soup").innerHTML = elementSoup;
+  });
   _$(".toggle-sushi").style.display = "none";
   _$(".toggle-appetizer").style.display = "none";
   _$(".toggle-soup").style.display = "none";
@@ -32,6 +156,42 @@ buttonSoup.addEventListener("click", function (e) {
   _$(".toggle-soup").style.display = "block";
 });
 buttonMainDishes.addEventListener("click", function (e) {
+  $.getJSON("db/mainDishes.json", function (data) {
+    const elementMain = data
+      .map((value) => {
+        return `
+      <div class="menus col-md-6 col-lg-4 d-flex ">
+        <div class="menu-img img" 
+        onclick="getImages('{{${value.images}}}')"
+        style="background-image: url(${value.images[0]});"></div>
+        <div class="text">
+          <div class="d-flex justify-content-between">
+            <div class="">
+              <h6 style="color: #E52B34;">${value.name}</h6>
+            </div>
+            <div class="one-forth">
+              <span style="font-size: 12px !important;" class="price">${
+                value.calo
+              }</span>
+            </div>
+          </div>
+          ${value.description
+            .map((data) => {
+              return `
+            <div class="d-flex justify-content-between">
+              <p><span style="font-size: 12px;">${data}</span></p>
+            </div>
+            `;
+            })
+            .join("")}
+        </div>
+      </div>
+      `;
+      })
+      .join("");
+
+    document.querySelector(".main-dishes").innerHTML = elementMain;
+  });
   _$(".toggle-sushi").style.display = "none";
   _$(".toggle-appetizer").style.display = "none";
   _$(".toggle-soup").style.display = "none";
@@ -41,6 +201,42 @@ buttonMainDishes.addEventListener("click", function (e) {
   _$(".toggle-main-dishes").style.display = "block";
 });
 buttonHotPot.addEventListener("click", function (e) {
+  $.getJSON("db/pre-orderHotPot.json", function (data) {
+    const elementHotPot = data
+      .map((value) => {
+        return `
+      
+      <div class="menus col-md-6 col-lg-4 d-flex ">
+        <div class="menu-img img" 
+        onclick="getImages('{{${value.images}}}')"
+        style="background-image: url(${value.images[0]});"></div>
+        <div class="text">
+          <div class="d-flex justify-content-between">
+            <div class="">
+              <h6 style="color: #E52B34;">${value.name}</h6>
+            </div>
+            <div class="one-forth">
+              <span style="font-size: 12px !important;" class="price">${
+                value.calo
+              }</span>
+            </div>
+          </div>
+          ${value.description
+            .map((data) => {
+              return `
+            <div class="d-flex justify-content-between">
+              <p><span style="font-size: 12px;">${data}</span></p>
+            </div>
+            `;
+            })
+            .join("")}
+        </div>
+      </div>
+      `;
+      })
+      .join("");
+    document.querySelector(".hot-pot").innerHTML = elementHotPot;
+  });
   _$(".toggle-sushi").style.display = "none";
   _$(".toggle-appetizer").style.display = "none";
   _$(".toggle-soup").style.display = "none";
@@ -50,6 +246,47 @@ buttonHotPot.addEventListener("click", function (e) {
   _$(".toggle-hot-pot").style.display = "block";
 });
 buttonItallap.addEventListener("click", function (e) {
+  $.getJSON("db/itallap.json", function (data) {
+    const elementItallap = data
+      .map((value) => {
+        return `
+      <div class="menus col-md-6 col-lg-4 d-flex ">
+        <div class="menu-img img buttonImages" 
+        onclick="getImages('{{${value.images}}}')"
+        style="background-image: url(${value.images[0]});"></div>
+        <div class="text">
+          <div class="">
+            <h6 style="color: #E52B34;">${value.name}</h6>
+          </div>
+          ${value.description
+            .map((data) => {
+              return `
+            <div class="">
+              <h6 style="color: #E52B34;">${data.title}</h6>
+            </div>
+            ${data.type
+              .map((type) => {
+                return `
+              <div class="d-flex justify-content-between">
+                <p><span style="font-size: 12px;">${type.name}</span></p>
+                
+                <div class="one-forth">
+                  <span style="font-size: 12px !important;" class="price">${type.calo}</span>
+                </div>
+              </div>
+              `;
+              })
+              .join("")}
+            `;
+            })
+            .join("")}
+        </div>
+      </div>
+      `;
+      })
+      .join("");
+    document.querySelector(".itallap").innerHTML = elementItallap;
+  });
   _$(".toggle-sushi").style.display = "none";
   _$(".toggle-appetizer").style.display = "none";
   _$(".toggle-soup").style.display = "none";
@@ -59,6 +296,42 @@ buttonItallap.addEventListener("click", function (e) {
   _$(".toggle-itallap").style.display = "block";
 });
 buttonDesszert.addEventListener("click", function (e) {
+  $.getJSON("db/desszert.json", function (data) {
+    const elementDesszert = data
+      .map((value) => {
+        return `
+      
+      <div class="menus col-md-6 col-lg-4 d-flex ">
+        <div class="menu-img img" 
+        onclick="getImages('{{${value.images}}}')"
+        style="background-image: url(${value.images[0]});"></div>
+        <div class="text">
+          <div class="d-flex justify-content-between">
+            <div class="">
+              <h6 style="color: #E52B34;">${value.name}</h6>
+            </div>
+            <div class="one-forth">
+              <span style="font-size: 12px !important;" class="price">${
+                value.calo
+              }</span>
+            </div>
+          </div>
+          ${value.description
+            .map((data) => {
+              return `
+            <div class="d-flex justify-content-between">
+              <p><span style="font-size: 12px;">${data}</span></p>
+            </div>
+            `;
+            })
+            .join("")}
+        </div>
+      </div>
+      `;
+      })
+      .join("");
+    document.querySelector(".desszert").innerHTML = elementDesszert;
+  });
   _$(".toggle-sushi").style.display = "none";
   _$(".toggle-appetizer").style.display = "none";
   _$(".toggle-soup").style.display = "none";
@@ -103,4 +376,3 @@ overlay.addEventListener("click", function (e) {
   $(".slick-slider").slick("unslick");
   overlay.style.display = "none";
 });
-
